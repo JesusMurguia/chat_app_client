4fdf6d9fc67b85f3b0fee52c56c6ab77f3c9c92c
Jesus Murguia <jesusmurguiafac@gmail.com>
2023-02-10 21:24:29 -0700

    room id displayed and tweaks

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,8 +1,12 @@
 import { Outlet } from "react-router-dom";
+import { selectCurrentRoom } from "../features/auth/authSlice";
+import { useSelector } from "react-redux";
 
 const Layout = () => {
+  const roomid = useSelector(selectCurrentRoom);
   return (
     <main>
+      <p>{roomid ? "Room ID: " + roomid : ""}</p>
       <Outlet />
     </main>
   );
--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -1,5 +1,16 @@
 import React from "react";
+import { selectCurrentRoom } from "../features/auth/authSlice";
+import { useSelector } from "react-redux";
 
 export const SideBar = () => {
-  return <div className="side-bar">SideBar</div>;
+  const roomid = useSelector(selectCurrentRoom);
+  return (
+    <div className="side-bar">
+      <div>
+        <h1>Welcome to the chat app</h1>
+        <h2>Invite people to join this room with the id: {roomid}</h2>
+        <p>You can start a conversation by clicking on any user in the left.</p>
+      </div>
+    </div>
+  );
 };
